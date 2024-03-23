db283441836528d6e051a9c318c7a74bc984c0aa
Harshitha73 <harshithachava@gmail.com>
2024-03-23 15:08:33 -0400

    edit quiz

--- src/Kanbas/Courses/Quizzes/index.tsx
+++ src/Kanbas/Courses/Quizzes/index.tsx
@@ -4,11 +4,9 @@ function Quizzes() {
     
     return (
         <>
-
+            <input type="text" placeholder="Search for Quiz" className="float-start"/>
             <span style={{ float: 'right', display: 'flex', gap: '10px' }}>
-                <label htmlFor="myInput"></label>
-                <input type="text" placeholder="Search for Quiz" />
-                <button style={{backgroundColor:'red', color:'white'}} >+ Quiz</button>
+                <button style={{backgroundColor:'red', color:'white', border:"none"}} >+ Quiz</button>
                 <button type="button" className="btn btn-outline-dark" style={{ borderRadius: '0%', maxHeight: 'fit-content' }}><FaEllipsisV /></button>
             </span>
             <br /><hr />
