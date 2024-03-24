b83f928f166a058ea1bf243b07ad1b4471bf250a
Harshitha73 <harshithachava@gmail.com>
2024-03-23 23:43:35 -0400

    Style Changes

--- src/Kanbas/Courses/buttons.tsx
+++ src/Kanbas/Courses/buttons.tsx
@@ -1,6 +1,6 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 import { Button } from "react-bootstrap";
-import { FaRegCheckCircle, FaEllipsisV} from "react-icons/fa";
+import { FaRegCheckCircle, FaEllipsisV, FaCaretDown} from "react-icons/fa";
 function Buttons(){
 return(
   <div className='d-none d-md-block'>
@@ -8,13 +8,16 @@ return(
     <Button variant="outline-secondary" style={{color:'black', backgroundColor:"lightgray"}}>Collapse All</Button>
     <Button variant="outline-secondary" style={{color:'black', backgroundColor:"lightgray"}}>View Progress</Button>
     <Dropdown>
-        <Button variant="outline-secondary" style={{color:'black'}}><FaRegCheckCircle style={{paddingRight:'2px'}}/>Publish All</Button>
-        <Dropdown.Toggle variant="outline-secondary" style={{color:'black'}}/>
+        <Button variant="outline-secondary" style={{color:'black'}}><FaRegCheckCircle style={{paddingRight:'2px'}}/>Publish All  
+        </Button>
+        <Dropdown.Toggle variant="outline-secondary" as={Button} id="dropdown-basic">
+            <FaCaretDown/>
+          </Dropdown.Toggle>
         <Dropdown.Menu>
          <Dropdown.Item>Unpublish All</Dropdown.Item>
         </Dropdown.Menu>
     </Dropdown>
-    <Button variant="danger">+ Module</Button>{' '}
+    <Button variant="danger">+ Module</Button>
     <button type="button" className="btn btn-outline-dark"><FaEllipsisV/></button>
   </div>
   </div>
